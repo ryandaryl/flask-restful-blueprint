896432257d8341c146bdd9894453033acdd71c83
ryandaryl <ryandarylmills@gmail.com>
2018-02-14 12:24:07 +0000

    Travis CI localhost.

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -4,6 +4,6 @@ from yaml import load
 def before_all(context):
     context.settings = load(open('features/conf.yaml').read())
     context.port = os.environ.get('PORT', 5000)
-    context.base_url = ':'.join([context.settings['base_url'], context.port])
+    context.base_url = '{}:{}'.format(context.settings['base_url'], context.port)
     context.headers = {}
     context.verify_ssl = True
