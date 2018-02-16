260b146b3c13858adbc9ddaad6ddac2b7d51ab23
ryandaryl <ryandarylmills@gmail.com>
2018-02-16 08:41:57 +0000

    Hard-code Gunicorn in tests.

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -3,7 +3,6 @@ from yaml import load
 
 def before_all(context):
     context.settings = load(open('features/conf.yaml').read())
-    context.port = os.environ.get('PORT')
-    context.base_url = '{}:{}'.format(context.settings['base_url'], context.port)
+    context.base_url = '{}:{}'.format(context.settings['host'], context.settings['port'])
     context.headers = {}
     context.verify_ssl = True
